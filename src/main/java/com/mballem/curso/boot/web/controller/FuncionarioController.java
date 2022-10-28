13e5acacc3b47bad682ae7bd5cf46bf1db223cd2
vitor.goncalves <vitor.goncalves@oabgo.org.br>
2022-10-27 23:00:01 -0300

    Implementações para listar os funcionários por data de entrada e saída, cargo, nome e também para realizar a edição e exclusão de funcionários.

--- src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
@@ -1,8 +1,10 @@
 package com.mballem.curso.boot.web.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mballem.curso.boot.domain.Cargo;
@@ -63,6 +66,26 @@ public class FuncionarioController {
 		funcionarioService.excluir(id);
 		attr.addFlashAttribute("success", "Funcionário removido com sucesso.");
 		return "redirect:/funcionario/listar";
+	}
+	
+	public String getPorNome(@RequestParam("nome") String nome, ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarPorNome(nome));
+		return "/funcionario/lista";
+	}
+	
+	@GetMapping("/buscar/cargo")
+	public String getPorCargo(@RequestParam("id") Long id, ModelMap modelMap) {
+		modelMap.addAttribute("funcionarios", funcionarioService.buscarPorCargo(id));
+		return "/funcionario/lista";
+	}
+	
+	@GetMapping("/buscar/data")
+	public String getPorDatas(
+			@RequestParam(name = "entrada", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate entrada,
+			@RequestParam("saida") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate saida,
+			ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarPorDatas(entrada, saida));
+		return "/funcionario/lista";
 	}
 	
 	@ModelAttribute("cargos")
